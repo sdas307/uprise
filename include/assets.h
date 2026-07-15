177d2933e52a88a6ec62ba6ef5f4fef84d3c1d63
kingxavid <sdas307@users.noreply.github.com>
2026-07-15 20:26:43 +0530

    bug fixes

--- include/assets.h
+++ include/assets.h
@@ -11,7 +11,7 @@ extern const char PATH_PIG_SHEET[];
 extern const char PATH_COW_SHEET[];
 
 extern const char PATH_MAP[];
-extern const char PATH_TERRAIN[];
+extern const char PATH_BASE[];
 extern const char PATH_FOREST[];
 extern const char PATH_WORLD_OBJECTS[];
 extern const char PATH_SPRITE_SHEET[];
