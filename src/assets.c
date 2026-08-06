07867db2ac160953b0d875ce1b2842f6b3f58de8
kingxavid <sdas307@users.noreply.github.com>
2026-08-06 14:56:07 +0530

    Asset pack upgraded. Made spritesheets out of raw assets.

--- src/assets.c
+++ src/assets.c
@@ -1,15 +1,15 @@
 #include "assets.h"
 #include "config.h"
 
-const char PATH_PLAYER_SHEET[] = "assets/player/player.png";
+const char PATH_PLAYER_SHEET[] = "assets/old_assets/player/player.png";
 
-const char PATH_CHICKEN_SHEET[] = "assets/animals/chicken/chicken.png";
-const char PATH_SHEEP_SHEET[] = "assets/animals/sheep/sheep.png";
-const char PATH_PIG_SHEET[] = "assets/animals/pig/pig.png";
-const char PATH_COW_SHEET[] = "assets/animals/cow/cow.png";
+const char PATH_CHICKEN_SHEET[] = "assets/old_assets/animals/chicken/chicken.png";
+const char PATH_SHEEP_SHEET[] = "assets/old_assets/animals/sheep/sheep.png";
+const char PATH_PIG_SHEET[] = "assets/old_assets/animals/pig/pig.png";
+const char PATH_COW_SHEET[] = "assets/old_assets/animals/cow/cow.png";
 
-const char PATH_MAP[] = "assets/maps/map1.png";
-const char PATH_BASE[] = "assets/maps/map_base.png";
-const char PATH_FOREST[] = "assets/maps/map_forest.png";
-const char PATH_WORLD_OBJECTS[] = "assets/maps/world_objects_transparent.png";
-const char PATH_SPRITE_SHEET[] = "assets/uprise_spritesheet/spritesheet.png";
+const char PATH_MAP[] = "assets/old_assets/maps/map1.png";
+const char PATH_BASE[] = "assets/old_assets/maps/map_base.png";
+const char PATH_FOREST[] = "assets/old_assets/maps/map_forest.png";
+const char PATH_WORLD_OBJECTS[] = "assets/old_assets/maps/world_objects_transparent.png";
+const char PATH_SPRITE_SHEET[] = "assets/old_assets/uprise_spritesheet/spritesheet.png";
