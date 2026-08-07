7b72b9afafba13082892d75e06235f41626fbc9a
kingxavid <sdas307@users.noreply.github.com>
2026-08-07 13:20:30 +0530

    New base data have been added.

---
+++ include/base2_data.h
@@ -0,0 +1,44 @@
+#ifndef BASE2_DATA_H
+#define BASE2_DATA_H
+
+#include "xEngine.h"
+
+#include "base2_data.h"
+
+extern const xVector2 grass1[];
+extern const int grass1Count;
+
+extern const xVector2 grass2[];
+extern const int grass2Count;
+
+extern const xVector2 grass3[];
+extern const int grass3Count;
+
+extern const xRectangle hedgeContinuous[];
+extern const int hedgeContinuousCount;
+
+extern const xVector2 hedges[];
+extern const int hedgesCount;
+
+extern const xRectangle highGround[];
+extern const int highGroundCount;
+
+extern const xVector2 highGroundStairs[];
+extern const int highGroundStairsCount;
+
+extern const xVector2 house[];
+extern const int houseCount;
+
+extern const xVector2 treeFruitLarge[];
+extern const int treeFruitLargeCount;
+
+extern const xVector2 wildflower1[];
+extern const int wildflower1Count;
+
+extern const xVector2 wildflower2[];
+extern const int wildflower2Count;
+
+extern const xVector2 wildflower3[];
+extern const int wildflower3Count;
+
+#endif
