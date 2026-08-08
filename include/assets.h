d706cf87c45271bb11d15dcdc03ed55b7f507a06
kingxavid <sdas307@users.noreply.github.com>
2026-08-08 14:13:36 +0530

    Minor bug fixes.

--- include/assets.h
+++ include/assets.h
@@ -15,24 +15,24 @@ extern const char PATH_WORLD_OBJECTS[];
 extern const char PATH_SPRITE_SHEET[];
 
 // Structure spritesheet paths
-const char PATH_HOUSE_WOOD_SPRITESHEET[];
-const char PATH_FENCE_POSTS_SPRITESHEET[];
-const char PATH_BRIDGE_WALLS_SPRITESHEET[];
+extern const char PATH_HOUSE_WOOD_SPRITESHEET[];
+extern const char PATH_FENCE_POSTS_SPRITESHEET[];
+extern const char PATH_BRIDGE_WALLS_SPRITESHEET[];
 
 // Nature spritesheet paths
-const char PATH_TREES_SPRITESHEET[];
-const char PATH_GRASSLANDS_SPRITESHEET[];
-const char PATH_GRASS_WILDFLOWERS_SPRITESHEET[];
-const char PATH_CROPS_FRUITS_SPRITESHEET[];
-const char PATH_FLOWER_MUSHROOMS_SPRITESHEET[];
-const char PATH_CLIFFS_SPRITESHEET[];
-const char PATH_WATER_SPRITESHEET[];
-const char PATH_WATERFALL_SPRITESHEET[];
+extern const char PATH_TREES_SPRITESHEET[];
+extern const char PATH_GRASSLANDS_SPRITESHEET[];
+extern const char PATH_GRASS_WILDFLOWERS_SPRITESHEET[];
+extern const char PATH_CROPS_FRUITS_SPRITESHEET[];
+extern const char PATH_FLOWER_MUSHROOMS_SPRITESHEET[];
+extern const char PATH_CLIFFS_SPRITESHEET[];
+extern const char PATH_WATER_SPRITESHEET[];
+extern const char PATH_WATERFALL_SPRITESHEET[];
 
 // Cave spritesheet paths
-const char PATH_CAVE_SPRITESHEET[];
+extern const char PATH_CAVE_SPRITESHEET[];
 
 // Weather spritesheet paths
-const char PATH_WEATHER_SPRITESHEET[];
+extern const char PATH_WEATHER_SPRITESHEET[];
 
 #endif
